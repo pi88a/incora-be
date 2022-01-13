c1a0503e151e39457f53dd9a6e4d2d7571068b7d
Taras Kosyi <taras.kosyy@gmail.com>
2022-01-13 13:22:18 +0200

    another attempt to enable playground in prod

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => ({
   //
   graphql: {
-    endpoint: "/graphql",
-    shadowCRUD: true,
-    playgroundAlways: true,
-    depthLimit: 100,
-    apolloServer: {
-      tracing: false,
-    },
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    }
   },
 });
