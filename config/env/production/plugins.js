ecb88f6428a7d896a4493dbbbb4a8bc4c859186c
Taras Kosyi <taras.kosyy@gmail.com>
2022-01-17 11:20:50 +0200

    config field

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,11 +1,13 @@
-module.exports = ({ env }) => ({
+module.exports = {
   graphql: {
-    endpoint: "/graphql",
-    shadowCRUD: true,
-    playgroundAlways: true,
-    depthLimit: 100,
-    apolloServer: {
-      tracing: false,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
     },
   },
-});
+};
