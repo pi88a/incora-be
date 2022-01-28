b762644c05fb158c04addc5b637b9dd3eeda8ea8
Taras Kosyi <taras.kosyy@gmail.com>
2022-01-28 12:11:39 +0200

    fixes: about, career pages

--- config/plugins.js
+++ config/plugins.js
@@ -30,9 +30,7 @@ module.exports = ({ env }) => ({
         region: env('AWS_REGION'),
         params: {
           Bucket: env('AWS_BUCKET_NAME'),
-           StorageClass: env('AWS_S3_STORAGE_CLASSES') // Configure your Amazon S3 Storage Classes (including this to environment variable is not a must)
         },
-        logger: console // Only if you want to `stdout` logs
       }
     }
   },
